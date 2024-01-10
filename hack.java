d45b605893835037207bfb0c6f29b336137f794f
Tharun Akuthota <124380030+Tharun-Akuthota@users.noreply.github.com>
2024-01-10 13:58:15 +0530

    Create hack.java

---
+++ hack.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class hack {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
