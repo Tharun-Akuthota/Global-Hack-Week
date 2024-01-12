1dc318354b47c8aa32a79672f6953ec91b61e806
Tharun Akuthota <124380030+Tharun-Akuthota@users.noreply.github.com>
2024-01-12 23:02:16 +0530

    Update hack.java

--- hack.java
+++ hack.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class hack {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -8,7 +7,6 @@ public class hack {
         for (int i = 0; i < n; i++) {
             array[i] = in.nextInt();
         }
-
         for (int i = 0; i < n; i++) {
             System.out.print(array[i] + " ");
         }
